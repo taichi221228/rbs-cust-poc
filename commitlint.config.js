a2faf4b2363da61b73b07cdd1555b9096a77627d
たいち ひ <taichi221228@icloud.com>
2024-10-19 14:43:28 +0900

    chore(deps): Add commitlint configuration and dependencies
    Integrate commitlint configuration and related dependencies for enforcing conventional commit messages.

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+/**
+ * @private
+ * @type {import('@commitlint/types').UserConfig}
+ */
+// HACK: Suppress TypeScript errors due to incompatibility between `eslint-plugin-import-access` and `tsc` when using `checkJS`.
+// @ts-expect-error eslint-disable-line @typescript-eslint/ban-ts-comment
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-case": [2, "always", "sentence-case"],
+    "body-max-line-length": [2, "always", Number.POSITIVE_INFINITY],
+  },
+};
