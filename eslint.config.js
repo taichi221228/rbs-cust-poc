ba20b18537b431de77fdea0e39df747afdfc3904
たいち ひ <taichi221228@icloud.com>
2024-10-20 11:31:10 +0900

    fix(linter): Fix order of ESLint configurations
    Reordered the inclusion of configurations to ensure the `ignoreConfig` is processed before `jsConfig` and TypeScript configurations.

--- eslint.config.js
+++ eslint.config.js
@@ -28,9 +28,9 @@ const tsConfigs = [
 // HACK: Suppress TypeScript errors due to incompatibility between `eslint-plugin-import-access` and `tsc` when using `checkJS`.
 // @ts-expect-error eslint-disable-line @typescript-eslint/ban-ts-comment
 export default defineConfig(
-  ...tsConfigs,
-  jsConfig,
   ignoreConfig,
+  jsConfig,
+  ...tsConfigs,
   {
     languageOptions: {
       globals: { ...globals.browser, ...globals.commonjs },
