9beca84b0072c0de049df144ae3376cb10acefa4
たいち ひ <taichi221228@icloud.com>
2024-10-21 09:51:42 +0900

    chore(panda): Add token configurations to panda.config
    Created a new file `panda.config.tokens.ts` to define and export tokens and semanticTokens using Panda CSS.

---
+++ panda.config.tokens.ts
@@ -0,0 +1,7 @@
+import { defineSemanticTokens, defineTokens } from "@pandacss/dev";
+
+/** @package */
+export const tokens = defineTokens({});
+
+/** @package */
+export const semanticTokens = defineSemanticTokens({});
--- panda.config.ts
+++ panda.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "@pandacss/dev";
 
+import { tokens, semanticTokens } from "./panda.config.tokens";
+
 /** @private */
 export default defineConfig({
 	preflight: true,
 	include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
 	exclude: [],
-	theme: {
-		extend: {},
-	},
+	theme: { tokens, semanticTokens },
 	outdir: "styled-system",
 });
