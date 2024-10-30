959ed10fc034862dc8ea99d793a086ecfe431902
たいち ひ <taichi221228@icloud.com>
2024-10-30 16:53:42 +0900

    fix(panda): Fix font name in tokens config
    Updated the font name for 'Josefin Sans' to 'Josefin Sans Variable' in the tokens configuration file.

--- panda.config.tokens.ts
+++ panda.config.tokens.ts
@@ -2,7 +2,7 @@ import { defineSemanticTokens, defineTokens } from "@pandacss/dev";
 
 /** @package */
 export const tokens = defineTokens({
-	fonts: { josefin: { value: "Josefin Sans" } },
+	fonts: { josefin: { value: '"Josefin Sans Variable"' } },
 });
 
 /** @package */
