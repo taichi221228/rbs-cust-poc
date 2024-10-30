92a0f9fcf7182900ee3356ccde3e09301c2a8b79
たいち ひ <taichi221228@icloud.com>
2024-10-30 15:41:53 +0900

    feat(panda): Add 'Josefin Sans' font token
    Updated the tokens configuration to include the 'Josefin Sans' font under the `fonts` category.

--- panda.config.tokens.ts
+++ panda.config.tokens.ts
@@ -1,7 +1,9 @@
 import { defineSemanticTokens, defineTokens } from "@pandacss/dev";
 
 /** @package */
-export const tokens = defineTokens({});
+export const tokens = defineTokens({
+	fonts: { josefin: { value: "Josefin Sans" } },
+});
 
 /** @package */
 export const semanticTokens = defineSemanticTokens({});
